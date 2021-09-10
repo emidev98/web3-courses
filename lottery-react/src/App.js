cc9620d70b521ec08944c7941db0135965cdce1c
Emilian Harastasan <eharastasan@gmail.com>
2021-09-10 18:37:03 +0200

    feat(react): basic web interaction with the contract

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,27 +1,111 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import web3 from './web3';
+import lottery from './lottery';
 
-function App() {
+class App extends React.Component {
+  state = {
+    loader : {
+      loading : true,
+      message : 'Loading...'
+    },
+    manager: '',
+    players: [],
+    balance: '',
+    value: 0
+  };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async componentDidMount() {
+    // TODO: Check if Metamask is already installed in the browser
+
+    this.setState({ 
+      manager: await lottery.methods.manager().call(),
+      players: await lottery.methods.getPlayers().call(),
+      balance: await web3.eth.getBalance(lottery.options.address)
+     });
+  }
+
+  onEnterToLottery = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    // TODO: Allow the user to chose betewen its different addresses 
+
+    this.setState({
+      loader : {
+        loading : true,
+        message : 'Waiting on transaction success...'
+      }
+    });
+
+    try{
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, 'ether')
+      })
+    }
+    catch(e){
+      console.log(e);
+      // TODO reset state
+    }
+
+    // TODO show the success state 
+
+    this.setState({
+      loader : {
+        loading : false,
+        message : ''
+      }
+    });
+  }
+
+  onPickWinner = async (event) => {
+    const accounts =  await web3.eth.getAccounts();
+    
+    this.setState({
+      loader : {
+        loading : true,
+        message : 'Waiting on transaction success...'
+      }
+    });
+
+    await lottery.methods.pickWinner().send({
+      from : accounts[0]
+    })
+
+    // TODO display the winner
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Lottery Contract</h2>
+          <p>This contract is managed by { this.state.manager }</p>
+          <p>There are currenly { this.state.players.length } people playing.</p>
+          <p>To win { web3.utils.fromWei(this.state.balance,'ether') }</p>
+        </div>
+
+        <form onSubmit={this.onEnterToLottery}>
+          <h4>Do you want to try your luck?</h4>
+          <div>
+            <label> Amount of ether to enter</label>
+            <input
+              value={this.state.value} 
+              onChange={ event => this.setState({ value: event.target.value })}>
+            </input>
+          </div>
+          <button>Enter</button>
+        </form>
+
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Ready to pick a winner?</h4>
+          <button onClick={this.onPickWinner}>Pick a winner!</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
